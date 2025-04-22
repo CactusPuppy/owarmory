3673e432db6a5e20fe1288e94f28740474ef0747
Mitchel <mitchel_jager@hotmail.com>
2025-04-22 23:14:31 +0200

    chore: Set up SCSS
    This sets up SCSS with some basic variables, functions, and mixins. The function in particular is just as a placeholder to prove that it works.
    
    All three files are imported globally as * so that we can use them without namespace and without having to reimport them.

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,14 @@ import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		css: {
+      includePaths: ['src'],
+      prependData: `
+        @use "src/lib/scss/_functions.scss" as *;
+      `,
+    },
+	}),
 	kit: { adapter: adapter() }
 };
 
