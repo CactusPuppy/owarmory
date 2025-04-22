79dd3091fa2a56b55d01e6f0c12edc07f6b8bf19
Mitchel <mitchel_jager@hotmail.com>
2025-04-23 01:05:54 +0200

    style: Set up base variables and styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+  import "$lib/scss/global.scss";
+
+  const { children }: { children: Snippet } = $props()
+</script>
+
+<main>
+  {@render children()}
+</main>
+
+<style lang="scss">
+  @use "sass:map";
+
+  main {
+    max-width: calc(map.get($breakpoints, page-max-width) + ($gutter * 2));
+    padding: 0 $gutter;
+    margin: 0 auto;
+  }
+</style>
