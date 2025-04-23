7de431f2d318b081b951cf7f31c690c7f0adc768
Mitchel <mitchel_jager@hotmail.com>
2025-04-23 14:59:17 +0200

    feat: Add item and power components
    This adds the components for hoverable items and powers. Both are separate components that are fairly similar is shape, but just different enough to warrant different components. They share two componts;
    - Popover, which is a hover tooltip. This does not yet contain any logic for better positioning when the tooltip falls offscreen (like on mobile).
    - SharedDetail, which is the description that appears in the Popover. This is made to look as close to the game as possible, but I didn't add quite everything yet, icons are missing.

---
+++ src/lib/components/common/Popover.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { fly } from "svelte/transition";
+
+  const { children, content } = $props()
+
+  let active = $state(false)
+</script>
+
+<div class="popover">
+  <button
+    class="toggle"
+    onmouseenter={() => active = true}
+    onmouseleave={() => active = false}
+    onclick={() => active = !active}>
+    {@render children()}
+  </button>
+
+  {#if active}
+    <div class="content" transition:fly={{ y: 10, duration: 50 }}>
+      {@render content()}
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .popover {
+    display: inline-block;
+    position: relative;
+  }
+
+  .toggle {
+    appearance: none;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    background: transparent;
+  }
+
+  .content {
+    z-index: 10;
+    position: absolute;
+    bottom: -1rem;
+    left: 50%;
+    transform: translateX(-50%) translateY(100%);
+    background: $color-bg-dark;
+    border-radius: $border-radius;
+    border-top: 2px solid $color-text-alt;
+    border-bottom: 2px solid $color-text-alt;
+  }
+</style>
