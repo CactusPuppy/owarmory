b4d85915954a3ce2c79081880f62ab503e7ef074
Mitchel <mitchel_jager@hotmail.com>
2025-04-23 02:40:43 +0200

    refactor: Move layout to separate folder
    This makes it possible to break out of the layout. The main style is still imported in the root layout file so that error pages inherant from it more easily.

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import Navigation from "$lib/components/layout/Navigation.svelte";
+	import PageBackground from "$lib/components/layout/PageBackground.svelte";
+
+  const { children }: { children: Snippet } = $props()
+</script>
+
+<Navigation />
+
+<main>
+  {@render children()}
+</main>
+
+<PageBackground />
+
+<style lang="scss">
+  @use "sass:map";
+
+  main {
+    max-width: calc(map.get($breakpoints, page-max-width) + ($gutter * 2));
+    padding: clamp($gutter, 10vw, 7.5rem) $gutter;
+    margin: 0 auto;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,8 @@
 <script lang="ts">
 	import type { Snippet } from "svelte";
   import "$lib/scss/global.scss";
-	import Navigation from "$lib/components/layout/Navigation.svelte";
-	import PageBackground from "$lib/components/layout/PageBackground.svelte";
 
   const { children }: { children: Snippet } = $props()
 </script>
 
-<Navigation />
-
-<main>
-  {@render children()}
-</main>
-
-<PageBackground />
-
-<style lang="scss">
-  @use "sass:map";
-
-  main {
-    max-width: calc(map.get($breakpoints, page-max-width) + ($gutter * 2));
-    padding: clamp($gutter, 10vw, 7.5rem) $gutter;
-    margin: 0 auto;
-  }
-</style>
+{@render children()}
