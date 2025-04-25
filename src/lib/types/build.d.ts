4d83874287b675d33dd54b7ae932c20f2986d673
Mitchel <mitchel_jager@hotmail.com>
2025-04-26 00:43:29 +0200

    feat: Add round info to build detail page
    This adds a static version of the round info to the detail page. The contains the individual rounds as well as the description. With a variant for mobile as well.
    
    Once again I made some assuptions for types and suchs. Feel free to totally ignore these, these were just temporary to make development life a little easier.

--- src/lib/types/build.d.ts
+++ src/lib/types/build.d.ts
@@ -1,9 +1,24 @@
-import type { HeroName } from "./hero"
+import type { HeroData } from "./hero"
 
 // Temporary type, will look different
 export type Build = {
-  hero: HeroName
+  hero: HeroData
   title: string
-  introduction?: string,
+  introduction?: string
+  description?: string
   author: User
+  roundInfos: RoundInfo[]
+}
+
+export type RoundInfo = {
+  id: string
+  note: string
+  sections: RoundInfoSection[]
+}
+
+export type RoundInfoSection = {
+  id: string
+  title: string
+  power: object | null
+  items: object[]
 }
