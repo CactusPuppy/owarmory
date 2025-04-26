acd0578e6af4ca17d394af6db026d9e5b784f6fe
Mitchel <mitchel_jager@hotmail.com>
2025-04-26 21:30:53 +0200

    feat: Allow popover to be closed via outside click or escape
    On desktop the interaction is a simple hover, and closing the popover doesn't require extra actions. With touch screens, however, that is a bit different. The popovers are opened on tap, and closed by tapping the same element.
    This PR adds another way, and that is by tapping anywhere but the element itself. Or pressing escape, for good measure.

---
+++ src/lib/actions/closeable.ts
@@ -0,0 +1,25 @@
+import type { Action } from "svelte/action";
+
+export const closeable: Action<Node, { onclose?: () => void }> = (node, { onclose = () => null } = {}) => {
+  function click(event: MouseEvent): void {
+    if (event.target instanceof Node && node.contains(event.target)) return;
+
+    onclose();
+  }
+
+  function keydown(event: KeyboardEvent): void {
+    if (event.code !== "Escape") return;
+
+    onclose();
+  }
+
+  window.addEventListener("click", click);
+  window.addEventListener("keydown", keydown);
+
+  return {
+    destroy() {
+      window.removeEventListener("click", click);
+      window.removeEventListener("keydown", keydown);
+    }
+  };
+};
