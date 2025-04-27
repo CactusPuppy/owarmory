577d5e99a56cb6c0601e60cc43297b9195cb50f7
Mitchel <mitchel_jager@hotmail.com>
2025-04-27 22:23:26 +0200

    feat: Add simple account page
    This account page shows the user's builds and a log out button. Not much else going on just yet!

---
+++ src/routes/(main)/account/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import BuildsList from "$lib/components/content/BuildsList.svelte";
+  import type { CurrentRound } from "$lib/types/round";
+  import { ROUND_MAX } from "$lib/constants/round";
+  import { getContext, setContext } from "svelte";
+  import type { User } from "$lib/types/user";
+  import LogoutButton from "$lib/components/content/auth/LogoutButton.svelte";
+
+  const currentUser: User = getContext("currentUser");
+  const currentRound: CurrentRound = $state({ value: ROUND_MAX });
+
+  setContext("currentRound", currentRound);
+
+</script>
+
+<svelte:head>
+  <title>Account | {currentUser.username} | OW Armory</title>
+</svelte:head>
+
+<header class="header">
+  <h1>Hey, {currentUser.username}</h1>
+
+  <LogoutButton />
+</header>
+
+<BuildsList header="Your Builds" />
+
+<style lang="scss">
+  h1 {
+    margin: 0;
+  }
+
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+
+    @include breakpoint(tablet) {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+</style>
