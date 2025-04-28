bba00c9320a485738fadb78417d4b1c4263080c2
Mitchel <mitchel_jager@hotmail.com>
2025-04-28 21:25:02 +0200

    chore: Prettier

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,35 +1,45 @@
 <footer class="footer">
   <div>
-    Join us on <a href="discord-link" target="_blank" rel="noreferrer noopener" aria-label="Discord (opens in new tab)">Discord</a>
+    Join us on <a
+      href="discord-link"
+      target="_blank"
+      rel="noreferrer noopener"
+      aria-label="Discord (opens in new tab)">Discord</a
+    >
   </div>
 
-	<div>
-		<a href="/privacy">Privacy</a> | Contribute on
-		<a href="https://github.com/CactusPuppy/owarmory/" target="_blank" rel="noreferrer noopener" aria-label="Github (opens in new tab)">GitHub</a>
-	</div>
+  <div>
+    <a href="/privacy">Privacy</a> | Contribute on
+    <a
+      href="https://github.com/CactusPuppy/owarmory/"
+      target="_blank"
+      rel="noreferrer noopener"
+      aria-label="Github (opens in new tab)">GitHub</a
+    >
+  </div>
 </footer>
 
 <style lang="scss">
   @use "sass:map";
 
   a {
-		color: $color-text-alt;
+    color: $color-text-alt;
 
-		&:hover {
-			color:$white;
-		}
-	}
+    &:hover {
+      color: $white;
+    }
+  }
 
-	.footer {
+  .footer {
     max-width: calc(map.get($breakpoints, page-max-width) + $gutter * 2);
     padding: 0.75rem $gutter;
-		margin: $vertical-offset-large auto 0;
-		color: $color-text-alt;
-		font-size: $font-size-small;
+    margin: $vertical-offset-large auto 0;
+    color: $color-text-alt;
+    font-size: $font-size-small;
 
-		@include breakpoint(tablet) {
-			display: flex;
-			justify-content: space-between;
-		}
-	}
+    @include breakpoint(tablet) {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 </style>
