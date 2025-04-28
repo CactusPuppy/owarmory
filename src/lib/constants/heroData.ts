4954b2913e0e9804e4be9eff591bc5966ffddd27
CactusPuppy <puppycactus@gmail.com>
2025-04-27 21:26:09 -0700

    chore: add initial db seed script

---
+++ src/lib/constants/heroData.ts
@@ -0,0 +1,72 @@
+import type { HeroData } from "../types/hero";
+
+export const heroes: HeroData[] = [
+  {
+    name: "D.Va",
+    role: "Tank",
+  },
+  {
+    name: "Junker Queen",
+    role: "Tank",
+  },
+  {
+    name: "Orisa",
+    role: "Tank",
+  },
+  {
+    name: "Reinhardt",
+    role: "Tank",
+  },
+  {
+    name: "Zarya",
+    role: "Tank",
+  },
+  {
+    name: "Ashe",
+    role: "Damage",
+  },
+  {
+    name: "Cassidy",
+    role: "Damage",
+  },
+  {
+    name: "Genji",
+    role: "Damage",
+  },
+  {
+    name: "Mei",
+    role: "Damage",
+  },
+  {
+    name: "Reaper",
+    role: "Damage",
+  },
+  {
+    name: "Soldier: 76",
+    role: "Damage",
+  },
+  {
+    name: "Ana",
+    role: "Support",
+  },
+  {
+    name: "Juno",
+    role: "Support",
+  },
+  {
+    name: "Kiriko",
+    role: "Support",
+  },
+  {
+    name: "Lúcio",
+    role: "Support",
+  },
+  {
+    name: "Mercy",
+    role: "Support",
+  },
+  {
+    name: "Moira",
+    role: "Support",
+  },
+];
