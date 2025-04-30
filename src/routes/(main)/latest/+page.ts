53a67cb0889f3c440b55c1f163ca69ea1fae6c98
CactusPuppy <puppycactus@gmail.com>
2025-04-30 09:39:52 -0700

    refactor: use existing queryParams arg for api call

--- src/routes/(main)/latest/+page.ts
+++ src/routes/(main)/latest/+page.ts
@@ -1,9 +1,9 @@
-import type { Build } from "$lib/types/build.js";
+import type { FullStadiumBuild as Build } from "$lib/types/build";
 import { api } from "$lib/utils/api.js";
 
 export async function load({ url, fetch }) {
   const page = parseInt(url.searchParams.get("page") || "1");
-  const builds = (await api<Build[]>(`builds/latest?page=${page}`, fetch)) || [];
+  const builds = (await api<Build[]>(`builds/latest?page=${page}`, {}, fetch)) || [];
 
   return {
     builds,
