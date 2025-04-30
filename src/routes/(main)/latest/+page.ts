ad15bc0943384ac49187be6cd5aee8f4255cebbc
CactusPuppy <puppycactus@gmail.com>
2025-04-30 09:46:27 -0700

    fix: ensure latest builds pages start from the beginning

--- src/routes/(main)/latest/+page.ts
+++ src/routes/(main)/latest/+page.ts
@@ -3,7 +3,9 @@ import { api } from "$lib/utils/api.js";
 
 export async function load({ url, fetch }) {
   const page = parseInt(url.searchParams.get("page") || "1");
-  const builds = (await api<Build[]>(`builds/latest?page=${page}`, {}, fetch)) || [];
+  // subtracting one since the API endpoint expects zero-indexing
+  const builds =
+    (await api<Build[]>(`builds/latest`, { page: (page - 1).toString(10) }, fetch)) || [];
 
   return {
     builds,
