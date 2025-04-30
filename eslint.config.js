12800f42ac47ae7b62dc920583352b149905ca1b
CactusPuppy <puppycactus@gmail.com>
2025-04-29 22:50:58 -0700

    chore: allow underscored variables to be unused

--- eslint.config.js
+++ eslint.config.js
@@ -38,6 +38,10 @@ export default ts.config(
           destructuring: "all",
         },
       ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
     },
   },
   {
