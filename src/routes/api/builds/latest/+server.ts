6af10d7918ecb2fedd800fde5f6a69bbedcfe8d9
Mitchel <mitchel_jager@hotmail.com>
2025-05-02 01:44:25 +0200

    chore: Use const for page size

--- src/routes/api/builds/latest/+server.ts
+++ src/routes/api/builds/latest/+server.ts
@@ -1,11 +1,13 @@
 import { prisma } from "$src/database/prismaClient.server.js";
 import { FullStadiumBuildInclude } from "$src/lib/types/build";
+import { BUILDS_PAGE_SIZE } from "$src/lib/types/page.js";
 
 export async function GET({ url }) {
   const headers = { "Content-Type": "application/json" };
-  const page = Number.parseInt(url.searchParams.get("page") ?? "0", 10) || 0;
+  const page = Number.parseInt(url.searchParams.get("page") ?? "0", BUILDS_PAGE_SIZE) || 0;
   const MAX_PAGE_SIZE = 100;
-  let PAGE_SIZE = Number.parseInt(url.searchParams.get("page_size") ?? "", 10) || 10;
+  let PAGE_SIZE =
+    Number.parseInt(url.searchParams.get("page_size") ?? "", BUILDS_PAGE_SIZE) || BUILDS_PAGE_SIZE;
   if (PAGE_SIZE > MAX_PAGE_SIZE) PAGE_SIZE = MAX_PAGE_SIZE;
 
   const builds = await prisma.stadiumBuild.findMany({
