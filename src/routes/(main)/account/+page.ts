c0cf6a01e5de441a48480f9f1098a6d1a2de0de7
Mitchel <mitchel_jager@hotmail.com>
2025-05-01 17:45:50 +0200

    fix: Redirect away from account page if not signed in

--- src/routes/(main)/account/+page.ts
+++ src/routes/(main)/account/+page.ts
@@ -1,7 +1,11 @@
 import type { FullStadiumBuild as Build } from "$src/lib/types/build";
 import { api } from "$lib/utils/api";
+import { redirect } from "@sveltejs/kit";
 
-export async function load({ fetch }) {
+export async function load({ fetch, parent }) {
+  const { currentUser } = await parent();
+  if (!currentUser) redirect(303, "/");
+
   const latestUserBuilds = (await api<Build[]>("builds/user", {}, fetch)) || [];
 
   return {
