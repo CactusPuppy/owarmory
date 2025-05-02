663a4fabacb1c6f9b040f8773e7ece7fd5d8677e
CactusPuppy <puppycactus@gmail.com>
2025-05-02 12:47:41 -0700

    fix(railway/auth.js): ensure railway proxy host is trusted

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,16 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import BattleNet from "@auth/sveltekit/providers/battlenet";
-import { AUTH_BATTLENET_CLIENT_ID, AUTH_BATTLENET_CLIENT_SECRET } from "$env/static/private";
+import {
+  AUTH_BATTLENET_CLIENT_ID,
+  AUTH_BATTLENET_CLIENT_SECRET,
+  AUTH_TRUST_HOST,
+} from "$env/static/private";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "$src/database/prismaClient.server";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: !!AUTH_TRUST_HOST,
   providers: [
     BattleNet({
       clientId: AUTH_BATTLENET_CLIENT_ID,
