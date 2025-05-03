23d2f1b288f6d8f4595c685072ccadfec44b165b
Mitchel <mitchel_jager@hotmail.com>
2025-05-03 15:53:29 +0200

    style: Add backdrops to purchase orders
    Just to make them stand out a little more, and have them fit in a little better.

--- src/lib/components/content/BuildItemOrder.svelte
+++ src/lib/components/content/BuildItemOrder.svelte
@@ -50,6 +50,9 @@
     align-items: center;
     flex-wrap: wrap;
     gap: 0.25rem;
+    padding: 0.75rem;
+    border-radius: $border-radius-small;
+    background: $color-bg-dark;
   }
 
   .cell {
