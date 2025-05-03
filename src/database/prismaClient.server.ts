769b657b8287fffa13260289b1dfd61af094e0f7
CactusPuppy <puppycactus@gmail.com>
2025-05-02 17:09:48 -0700

    test: initial e2e testing for powers API

--- src/database/prismaClient.server.ts
+++ src/database/prismaClient.server.ts
@@ -1,5 +1,8 @@
-import { DATABASE_URL } from "$env/static/private";
+// import { DATABASE_URL } from "$env/static/private";
+import "dotenv/config";
 import { PrismaClient } from "$src/generated/prisma";
+
+const DATABASE_URL = process.env.DATABASE_URL;
 
 export const prisma = new PrismaClient({
   datasourceUrl: DATABASE_URL,
