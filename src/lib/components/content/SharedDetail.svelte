bc8638580c5e477ffd7c4627dff89657a80be839
Mitchel <mitchel_jager@hotmail.com>
2025-05-03 14:20:19 +0200

    feat: Mark numbers in descriptions for readability
    To match the in-game formatting, numbers in the description of items are wrapped with <mark>, turning them white.

--- src/lib/components/content/SharedDetail.svelte
+++ src/lib/components/content/SharedDetail.svelte
@@ -12,8 +12,21 @@
 
   const { name, description, cost = 0, statMods }: Props = $props();
 
+  function parseDescription(text: string): string {
+    let output = wrapBracketsWithMark(text);
+    output = wrapNumbersWithMark(output);
+
+    return output;
+  }
+
+  /** Replace [word] with <mark>[word]</mark> */
   function wrapBracketsWithMark(text: string): string {
     return text.replace(/\[[^\]]+\]/g, (match) => `<mark>${match}</mark>`);
+  }
+
+  /** Replace 10, 10%, 10s, and 10m with <mark>{match}<mark> */
+  function wrapNumbersWithMark(text: string): string {
+    return text.replace(/\d+(?:\.\d+)?(?:%|s|m)?/g, "<mark>$&</mark>");
   }
 </script>
 
@@ -24,7 +37,7 @@
 
   {#if description}
     <!-- eslint-disable-next-line svelte/no-at-html-tags This should be safe, right? The input is determined by us. -->
-    <p class="description">{@html wrapBracketsWithMark(description)}</p>
+    <p class="description">{@html parseDescription(description)}</p>
   {/if}
 
   {#if statMods?.length}
