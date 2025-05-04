24150d6d46baf0d90a40ad1d2b74ec6d257b0fc6
CactusPuppy <puppycactus@gmail.com>
2025-05-04 09:24:14 -0700

    fix: ensure imageSize is used

--- src/lib/components/content/Item.svelte
+++ src/lib/components/content/Item.svelte
@@ -41,7 +41,7 @@
   <Popover onclick={() => onclick(item)}>
     <div class="item {rarity.toLowerCase()}" class:large class:sold>
       <div class="icon">
-        <img src={iconURL} alt={name} />
+        <img src={iconURL} alt={name} height={imageSize} />
       </div>
 
       {#if sold}
