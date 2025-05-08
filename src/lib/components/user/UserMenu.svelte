f13d8d208cfed034eb8803c07848cd876ef08ed7
Mitchel Jager <mitchel_jager@hotmail.com>
2025-05-08 20:28:26 +0200

    style: Mobile navigation (#82)
    ## Description
    
    The currently navigation did not fit on mobile. I still wanted to keep it simple, so I shrank down everything a bit, hiding things that are too large, replacing them with icons. Not the best solution, and it would be nicer to move things to a proper fold out menu, but that's for another time.
    
    ## Screenshots
    
    This is a pretty small size, smaller than most modern phones.
    
    Signed out | Signed in
    --- | ---
    ![image](https://github.com/user-attachments/assets/8d8c636a-6998-433c-9b72-298b5d3f4496) | ![image](https://github.com/user-attachments/assets/9efb513e-3787-4c6a-9f60-968d953a247c)

--- src/lib/components/user/UserMenu.svelte
+++ src/lib/components/user/UserMenu.svelte
@@ -2,12 +2,19 @@
   import type { User } from "$src/generated/prisma";
   import { cleanName } from "$src/lib/utils/user";
   import { getContext } from "svelte";
+  import iconUser from "$lib/images/icons/user.svg";
 
   const currentUser: User = getContext("currentUser");
 </script>
 
 <div class="user">
-  Signed in as <a href="/account">{cleanName(currentUser.name!)}</a>
+  <span class="user__desktop">
+    Signed in as <a href="/account">{cleanName(currentUser.name!)}</a>
+  </span>
+
+  <a href="/account" class="user__mobile">
+    <img src={iconUser} alt={currentUser.name} height="40" width="40" />
+  </a>
 </div>
 
 <style lang="scss">
@@ -18,5 +25,27 @@
   .user {
     white-space: nowrap;
     font-size: $font-size-small;
+  }
+
+  .user__mobile {
+    display: flex;
+    align-items: center;
+
+    &:hover,
+    &:active {
+      filter: brightness(100);
+    }
+
+    @include breakpoint(tablet) {
+      display: none;
+    }
+  }
+
+  .user__desktop {
+    display: none;
+
+    @include breakpoint(tablet) {
+      display: block;
+    }
   }
 </style>
