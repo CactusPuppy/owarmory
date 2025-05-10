c1f4c64c7b54c02873ee0cf3902039656613f13a
Mitchel Jager <mitchel_jager@hotmail.com>
2025-05-10 20:30:45 +0200

    feat: Share builds (#109)
    ## Description
    
    A simple input-like share box with a button to easily copy the URL. The input itself also automatically selects all text when you focus it.
    
    ## Screenshots
    
    ![share](https://github.com/user-attachments/assets/4735fc3d-de0e-4f48-a86c-ea5bd52901cb)

---
+++ src/lib/components/form/ShareInput.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { scale } from "svelte/transition";
+
+  const { path = "" } = $props();
+
+  const fullPath = $derived(page.url.origin + path);
+
+  let showSuccess = $state(false);
+
+  function selectAll(event: FocusEvent) {
+    (event.target as HTMLInputElement).select();
+  }
+
+  function copy() {
+    navigator.clipboard.writeText(fullPath);
+
+    showSuccess = true;
+    setTimeout(() => (showSuccess = false), 500);
+  }
+</script>
+
+<label class="form-label" for="share">Share</label>
+
+<div class="form-group">
+  <input type="text" class="form-input" id="share" readonly value={fullPath} onfocus={selectAll} />
+
+  <button class="button button--secondary" onclick={copy}>
+    Copy
+
+    {#if showSuccess}
+      <div class="success" transition:scale={{ duration: 100, start: 0.75 }}>Copied!</div>
+    {/if}
+  </button>
+</div>
+
+<style lang="scss">
+  .form-group {
+    display: flex;
+    gap: 0.25rem;
+    padding: 0.25rem;
+    background: $color-bg-light;
+    border-radius: $border-radius-small;
+  }
+
+  .form-input {
+    padding: 0.5rem 0 0.5rem 0.5rem;
+    font-size: $font-size-small;
+    color: $color-text-alt;
+  }
+
+  .button {
+    position: relative;
+  }
+
+  .success {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    border-radius: $border-radius-small * 0.5;
+    background: $green;
+    color: $white;
+  }
+</style>
