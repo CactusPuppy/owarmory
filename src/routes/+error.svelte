d72a6986e30e34b7a845084289ec90f5a1056280
CactusPuppy <puppycactus@gmail.com>
2025-05-11 23:32:42 -0700

    refactor: move to top-level and add "back to home" button

---
+++ src/routes/+error.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<div class="full-page">
+  <main>
+    <h1 class="error-title">{page.status}</h1>
+    <p class="error-message">{page.error?.message}</p>
+    <p class="home-button"><a href="/" class="button">Back to Home</a></p>
+  </main>
+</div>
+
+<style lang="scss">
+  .full-page {
+    width: 100dvw;
+    height: 100dvh;
+    align-content: center;
+  }
+
+  .error-title {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    text-align: center;
+  }
+
+  .error-message {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.25rem;
+  }
+
+  .home-button {
+    margin-top: 2rem;
+    text-align: center;
+    box-sizing: border-box;
+  }
+</style>
