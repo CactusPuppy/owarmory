a3a067b4d38411173d9ffd0d84242ca2a4347843
CactusPuppy <puppycactus@gmail.com>
2025-05-11 22:59:27 -0700

    fix: gracefully handle builds which have not been found

---
+++ src/routes/(main)/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<h1 class="error-title">{page.status}</h1>
+<p class="error-message">{page.error?.message}</p>
+
+<style lang="scss">
+  .error-title {
+    text-align: center;
+  }
+
+  .error-message {
+    text-align: center;
+  }
+</style>
--- src/routes/(main)/build/[id]-[slug]/+layout.ts
+++ src/routes/(main)/build/[id]-[slug]/+layout.ts
@@ -5,9 +5,15 @@ import { toSlug } from "$src/lib/utils/slug.js";
 import { error, redirect } from "@sveltejs/kit";
 
 export async function load({ params, fetch }) {
-  const build = await api<FullStadiumBuild>(`/build/${params.id}`, {}, fetch);
-
-  if (!build) error(404, "Build not found");
+  let build: FullStadiumBuild;
+  try {
+    build = await api<FullStadiumBuild>(`/build/${params.id}`, {}, fetch);
+  } catch (e) {
+    if (typeof e == "object" && e && "status" in e && e.status === 404) {
+      error(404, "Build not found");
+    }
+    throw e;
+  }
 
   // Slug in the param did not match the slug derived from the title.
   // Could be because it was entered in the URL wrong, or because the title changed.
