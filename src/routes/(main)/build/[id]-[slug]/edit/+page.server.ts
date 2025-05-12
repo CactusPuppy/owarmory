8300131fc0c39db9337eced2cebe7eb5b210ec35
CactusPuppy <puppycactus@gmail.com>
2025-05-12 12:00:17 -0700

    fix: ensure edit page redirects non-authorized users back to the build page

--- src/routes/(main)/build/[id]-[slug]/edit/+page.server.ts
+++ src/routes/(main)/build/[id]-[slug]/edit/+page.server.ts
@@ -1,14 +1,18 @@
 import type { Tag } from "$src/generated/prisma/client.js";
 import type { Item, Power } from "$src/generated/prisma/index.js";
-import type { BuildData } from "$src/lib/types/build.js";
+import type { FullStadiumBuild } from "$src/lib/types/build.js";
 import { isHeroName } from "$src/lib/types/hero.js";
 import { api } from "$src/lib/utils/api.js";
-import { error } from "@sveltejs/kit";
+import { buildPath } from "$src/lib/utils/routes.js";
+import { error, redirect } from "@sveltejs/kit";
 
-export async function load({ params, fetch }) {
-  const build = await api<BuildData>(`/build/${params.id}`, {}, fetch);
+export async function load({ locals, params, fetch }) {
+  const session = await locals.auth();
+  const user = session?.user;
+  const build = await api<FullStadiumBuild>(`/build/${params.id}`, {}, fetch);
 
   if (!isHeroName(build.heroName)) error(500, "Unknown hero name from build " + params.id);
+  if (!user || user.id !== build.authorId) redirect(303, buildPath(build));
   const [items, powers, tags] = await Promise.all([
     api<Item[]>("/talents/items", { hero: build.heroName }, fetch),
     api<Power[]>("/talents/powers", { hero: build.heroName }, fetch),
