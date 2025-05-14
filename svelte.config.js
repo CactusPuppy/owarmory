7c163b021380fe560c146212222ab50871d15344
CactusPuppy <puppycactus@gmail.com>
2025-05-13 22:02:59 -0700

    feat: add a basic CSP for defense-in-depth

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,15 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter(), alias: { $src: "src" } },
+  kit: {
+    adapter: adapter(),
+    alias: { $src: "src" },
+    csp: {
+      directives: {
+        "script-src": ["self"],
+      },
+    },
+  },
 };
 
 export default config;
