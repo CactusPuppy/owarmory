f0c10e8a21e2c2cda47b4c1e7f242198ca1e209f
Mitchel <mitchel_jager@hotmail.com>
2025-05-19 23:27:12 +0200

    feat: Add sitemap with generated paths
    Simple site map with our most important paths. The main goal here is to get every build indexed. We could consider adding user paths too at some point.

---
+++ src/routes/sitemap/+server.ts
@@ -0,0 +1,62 @@
+import { prisma } from "$src/database/prismaClient.server";
+import { buildPath, heroPath } from "$lib/utils/routes.js";
+import { heroes } from "$src/lib/constants/heroData.js";
+
+export async function GET({ url }) {
+  const host = url.origin;
+
+  const builds = await prisma.stadiumBuild.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 10000, // As many as we got, really. We're fine with slow responses here.
+  });
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+      <url>
+        <loc>https://${host}</loc>
+        <changefreq>daily</changefreq>
+      </url>
+
+      <url>
+        <loc>https://${host}/latest</loc>
+        <changefreq>daily</changefreq>
+      </url>
+
+      ${heroes
+        .map(
+          (hero) => `
+        <url>
+          <loc>https://${host}/${heroPath(hero.name)}</loc>
+          <changefreq>daily</changefreq>
+        </url>
+      `,
+        )
+        .join("")}
+
+      ${builds
+        .map(
+          (build) => `
+        <url>
+          <loc>https://${host}/${buildPath(build)}</loc>
+          <lastmod>${build.updatedAt}</lastmod>
+        </url>
+      `,
+        )
+        .join("")}
+    </urlset>
+  `;
+
+  return new Response(body, {
+    headers: {
+      "Cache-Control": "max-age=14400",
+      "Content-Type": "application/xml",
+    },
+  });
+}
