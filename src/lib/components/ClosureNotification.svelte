ccf491900fdc1aac117166125df7ea01439d5012
CactusPuppy <puppycactus@gmail.com>
2025-08-03 23:26:32 -0700

    feat: disable build creation and editing

---
+++ src/lib/components/ClosureNotification.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import WarningIcon from "$lib/images/icons/warning.svelte";
+</script>
+
+<div class="closure-notification">
+  <a href="/thank-you-whats-next" target="_blank" class="icon-container">
+    <WarningIcon />
+  </a>
+  <div>
+    <h3>OW Armory Is Shutting Down</h3>
+    <p>
+      Builds cannot be created or edited at this time. Your builds are being ported to
+      stadiumbuilds.io and will not be lost. If you do not want your build to be ported, you may
+      delete your post now to prevent that post from being transferred.<br />Thank you all for your
+      support over the past few months.
+    </p>
+    <a href="/thank-you-whats-next" target="_blank"><button class="button">Read More</button></a>
+  </div>
+</div>
+
+<style lang="scss">
+  @use "sass:color";
+  .closure-notification {
+    gap: 1.5rem;
+    align-items: center;
+    margin-top: $vertical-offset-md;
+    margin-bottom: calc($vertical-offset-md - $vertical-offset-large);
+    padding: $gutter;
+    border: 2px solid $primary;
+    border-radius: $border-radius;
+    background-color: color.adjust(color.change($primary, $alpha: 25%), $lightness: -25%);
+
+    @include breakpoint(mobile) {
+      display: flex;
+    }
+
+    .icon-container {
+      display: flex;
+      align-self: center;
+      justify-content: center;
+      width: calc($item-size * 2.5);
+      height: calc($item-size * 2.5);
+      flex-grow: 1;
+      margin-bottom: 1rem;
+
+      @include breakpoint(mobile) {
+        margin-bottom: 0;
+      }
+    }
+
+    h3 {
+      margin-bottom: 0;
+      margin-top: 0;
+    }
+
+    p {
+      color: $color-text-alt;
+    }
+  }
+</style>
---
+++ src/routes/(main)/thank-you-whats-next/+page.svelte
@@ -0,0 +1,36 @@
+<h1>Thank You, What's Next, and Goodbye</h1>
+<p>
+  While we've been thrilled with the reception OW Armory has received, unfortunately both of the
+  active developers of OW Armory have not found ourselves as passionate about Stadium as the rest of
+  the community has been. While we could continue to develop and maintain OW Armory, we'd be
+  delivering a half-baked product, which you, the community, do not deserve.
+</p>
+<p>
+  However, you all have put a humbling amount of work into making the builds on this site, and I'd
+  personally hate to see all that work go to waste. To that end, we've spoken with <a
+    href="https://stadiumbuilds.io"
+    target="_blank"
+    rel="noopener noreferrer">Stadium Builds</a
+  >, and they're willing to help us port all of your builds to their site!
+</p>
+<p>
+  So what's next? <em
+    >Effective immediately, new builds cannot be made, and existing builds cannot be edited.</em
+  >
+  This will ensure that we capture all the builds in our data export to Stadium Builds, and will
+  also ensure that everyone knows that Stadium Builds is the place to go to share builds. After the
+  build transfer is complete, <em>OW Armory will shut down</em> for good.
+</p>
+<p>
+  Thanks for the kind words and patronage over the past few months, but it's time to close the
+  armory and let you all get out into the fray.
+</p>
+<p>Humbly Yours,</p>
+<p>- CactusPuppy and Mitsiee</p>
+
+<style lang="scss">
+  em {
+    font-weight: bold;
+    color: $color-bg-hero;
+  }
+</style>
